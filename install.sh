e848a6a92dc7c0c8076941ad4b751ee88ac0ecee
wenlong_12 <wenlong12@huawei.com>
2023-04-26 06:51:53 +0000

    解压后重命名abseil-cpp
    Signed-off-by: wenlong_12 <wenlong12@huawei.com>

--- install.sh
+++ install.sh
@@ -8,9 +8,10 @@
 
 set -e
 cd $1
-if [ -d "abseil-cpp-20220623.1" ];then
-    rm -rf abseil-cpp-20220623.1
+if [ -d "abseil-cpp" ];then
+    rm -rf abseil-cpp
 fi
 tar zxvf abseil-cpp-20220623.1.tar.gz
-cd $1/abseil-cpp-20220623.1
+mv abseil-cpp-20220623.1 abseil-cpp
+cd $1/abseil-cpp
 exit 0
